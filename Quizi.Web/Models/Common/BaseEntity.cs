f3e5b39f18d06c566922fa15a21fc994a891aed8
Arian <153910642+DevRepoNull@users.noreply.github.com>
2026-07-15 18:36:30 +0330

    طراحی دیتابیس پروژه
    طراحی جدول مشترک
    طراحی دایرکتوری های دیتابیس

---
+++ Quizi.Web/Models/Common/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿namespace Quizi.Web.Models.Common
+{
+    public class BaseEntity
+    {
+        public Guid Id { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+        public DateTime CreatedDate { get; set; } = DateTime.Now;
+
+        public DateTime? UpdatedDate { get; set; }
+
+        public DateTime? DeletedDate { get; set; }
+    }
+}
